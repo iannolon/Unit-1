08638ddf5b26304abd3e5252f7f05ee4bf668d37
iannolon <35769152+iannolon@users.noreply.github.com>
2018-01-25 08:36:14 -0500

    Create nameAge.py

---
+++ nameAge.py
@@ -0,0 +1,10 @@
+#SwollenNolon
+#1/24/18
+#nameAge.py
+
+name = input('Enter your first and last name.')
+age = int(input('Enter your age.'))
+firstname, lastname = name.split()
+print ('Your first name has', len(firstname),'letters')
+print ('Your last name has', len(lastname), 'letters')
+print ('Next year you will be', age + 1, 'years old')
