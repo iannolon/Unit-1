ea6659ad3030af706ead60bc571e0168e8fe5458
iannolon <35769152+iannolon@users.noreply.github.com>
2018-01-25 08:40:53 -0500

    Create tipCalculator.py

---
+++ tipCalculator.py
@@ -0,0 +1,7 @@
+#SwollenNolon
+#1/24/18
+#tipCalculator.py
+
+baseprice = float(input('How much did the meal cost?'))
+tip = float(input('How much do you want to tip? (as a percent)'))
+print ('You should tip', baseprice * tip * 0.01, 'dollars.')
