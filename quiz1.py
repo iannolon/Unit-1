87f4e2b23e5b9eff9c9d5eac2ee29fc6b7d530e0
iannolon <35769152+iannolon@users.noreply.github.com>
2018-01-26 08:24:39 -0500

    Create quiz1.py

---
+++ quiz1.py
@@ -0,0 +1,13 @@
+#IanNolon
+#1/26/18
+#quiz1.py
+
+print ('Hello')
+name = input('What is your name?')
+favnum = int(input('What is your favorite number?'))
+nameprint = 0
+If nameprint <= favnum
+    print name
+    nameprint = nameprint + 1
+num2 = int(input ('What is your second favorite number?'))
+print ('Your lucky number is',favnum**num2)
